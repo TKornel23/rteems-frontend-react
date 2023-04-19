4de329b330f421eb09c010a5936fd4ad6fe23c5f
Kornél Tóth <tkornel23@gmail.com>
2023-04-19 23:42:47 +0200

    asd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
+import Img1 from '../assets/images/logo.svg'
+
 function Navbar() {
-
-
   
   return (
     <>     
 <nav class="shadow-md border-gray-200 sticky">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between p-1">
-      <img src="src\assets\images\logo.svg" class="h-16 mr-3" alt="RTEEMS Logo" />
+      <img src={Img1} class="h-16 mr-3" alt="RTEEMS Logo" />
     <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-black rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
       <span class="sr-only">Menu</span>
       <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Img1 from '../assets/images/background.svg'
 
 function Dashboard() {
   return (
     <>
-        <div className='bg-dashboardwhite bg-cover bg-repeat w-full h-full overflow-hidden scroll-smooth'>
+        <div className={`bg-[url('${Img1}')] bg-cover bg-repeat w-full h-full overflow-hidden scroll-smooth`}>
             <div className='grid xl:grid-cols-3 sm:grid-cols-2 gap-4 py-8 px-8 grid-cols-1 items-center m-auto'>
                 <div className='rounded-md'>
                     <iframe src="http://localhost:3000/d-solo/eNgGSGBVk/bird-temp?orgId=1&refresh=1m&var-bucket=8021220&theme=light&panelId=2" className='w-[350px] h-[300px] shadow-md lg:w-[450px] lg:h-[400] rounded-md m-auto' frameborder="0"></iframe>
